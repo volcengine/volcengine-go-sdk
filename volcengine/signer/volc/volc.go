7b184bbe44e663af471751ab99ff158f93d1b745
xuyaming <xuyaming@bytedance.com>
2022-08-30 15:11:46 +0800

    feat: support error return when use DynamicCredentials

--- volcengine/signer/volc/volc.go
+++ volcengine/signer/volc/volc.go
@@ -16,29 +16,29 @@ func SignSDKRequest(req *request.Request) {
 	region := req.ClientInfo.SigningRegion
 
 	var (
-		_credentials *credentials.Credentials
-		_region      *string
-		c1           base.Credentials
-		_err         error
+		dynamicCredentials *credentials.Credentials
+		dynamicRegion      *string
+		c1                 base.Credentials
+		err                error
 	)
 
 	if req.Config.DynamicCredentialsIncludeError != nil {
-		_credentials, _region, _err = req.Config.DynamicCredentialsIncludeError(req.Context())
-		if _err != nil {
-			req.Error = _err
+		dynamicCredentials, dynamicRegion, err = req.Config.DynamicCredentialsIncludeError(req.Context())
+		if err != nil {
+			req.Error = err
 			return
 		}
 	} else if req.Config.DynamicCredentials != nil {
-		_credentials, _region = req.Config.DynamicCredentials(req.Context())
+		dynamicCredentials, dynamicRegion = req.Config.DynamicCredentials(req.Context())
 	}
 
 	if req.Config.DynamicCredentials != nil {
-		_credentials, _region = req.Config.DynamicCredentials(req.Context())
-		if volcengine.StringValue(_region) == "" {
+		dynamicCredentials, dynamicRegion = req.Config.DynamicCredentials(req.Context())
+		if volcengine.StringValue(dynamicRegion) == "" {
 			req.Error = volcengine.ErrMissingRegion
 			return
 		}
-		region = volcengine.StringValue(_region)
+		region = volcengine.StringValue(dynamicRegion)
 	} else if region == "" {
 		region = volcengine.StringValue(req.Config.Region)
 	}
@@ -48,10 +48,10 @@ func SignSDKRequest(req *request.Request) {
 		name = req.ClientInfo.ServiceID
 	}
 
-	if _credentials == nil {
+	if dynamicCredentials == nil {
 		c1 = req.Config.Credentials.GetBase(region, name)
 	} else {
-		c1 = _credentials.GetBase(region, name)
+		c1 = dynamicCredentials.GetBase(region, name)
 	}
 	r := c1.Sign(req.HTTPRequest)
 	req.HTTPRequest.Header = r.Header
