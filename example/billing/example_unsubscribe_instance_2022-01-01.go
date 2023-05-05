29c45f1c8eefbe6396445881619744a03f355cc6
xuyaming0800 <xuyaming@bytedance.com>
2023-05-05 15:29:45 +0800

    feat: auto generate for xuyaming-20230505-billing

---
+++ example/billing/example_unsubscribe_instance_2022-01-01.go
@@ -0,0 +1,33 @@
+package billingexample
+
+import (
+	"fmt"
+
+	"github.com/volcengine/volcengine-go-sdk/service/billing"
+	"github.com/volcengine/volcengine-go-sdk/volcengine"
+	"github.com/volcengine/volcengine-go-sdk/volcengine/credentials"
+	"github.com/volcengine/volcengine-go-sdk/volcengine/session"
+)
+
+func UnsubscribeInstance() {
+	ak, sk, region := "Your AK", "Your SK", "Region"
+	config := volcengine.NewConfig().
+		WithRegion(region).
+		WithCredentials(credentials.NewStaticCredentials(ak, sk, ""))
+	sess, err := session.NewSession(config)
+	if err != nil {
+		panic(err)
+	}
+	svc := billing.New(sess)
+	unsubscribeInstanceInput := &billing.UnsubscribeInstanceInput{
+		ClientToken:                volcengine.String("uuid-xxxxxxxxxxxx"),
+		Product:                    volcengine.String("volume"),
+		UnsubscribeRelatedInstance: volcengine.Bool(true),
+	}
+
+	resp, err := svc.UnsubscribeInstance(unsubscribeInstanceInput)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp)
+}
