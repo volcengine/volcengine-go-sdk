fd9d7a658d4dcfde68b4b18782d1c4a76e9fc032
liuminghao <liuminghao.lm@bytedance.com>
2024-11-25 20:23:12 +0800

    feat(ark-context): update prompt token details

--- service/arkruntime/model/common.go
+++ service/arkruntime/model/common.go
@@ -21,15 +21,15 @@ const (
 	ErrorRetryMaxDelay  = 8 * time.Second
 )
 
-type TokensDetail struct {
+type PromptTokensDetail struct {
 	CachedTokens int `json:"cached_tokens"`
 }
 
 type Usage struct {
-	PromptTokens        int           `json:"prompt_tokens"`
-	CompletionTokens    int           `json:"completion_tokens"`
-	TotalTokens         int           `json:"total_tokens"`
-	PromptTokensDetails *TokensDetail `json:"prompt_tokens_details,omitempty"`
+	PromptTokens        int                `json:"prompt_tokens"`
+	CompletionTokens    int                `json:"completion_tokens"`
+	TotalTokens         int                `json:"total_tokens"`
+	PromptTokensDetails PromptTokensDetail `json:"prompt_tokens_details"`
 }
 
 type Response interface {
