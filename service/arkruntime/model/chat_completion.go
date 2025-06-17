5b0a4e6db96ad3e6dfb73208ec1e0d744e7752c8
潘婉宁 <panwanning@bytedance.com>
2025-06-17 17:36:40 +0800

    feat: add max_completion_tokens

--- service/arkruntime/model/chat_completion.go
+++ service/arkruntime/model/chat_completion.go
@@ -175,29 +175,30 @@ func (r ChatCompletionRequest) GetModel() string {
 // an empty string (""), false, or other zero values, it will be sent to
 // the server. The server will handle these fields according to the specified values.
 type CreateChatCompletionRequest struct {
-	Model             string                   `json:"model"`
-	Messages          []*ChatCompletionMessage `json:"messages"`
-	MaxTokens         *int                     `json:"max_tokens,omitempty"`
-	Temperature       *float32                 `json:"temperature,omitempty"`
-	TopP              *float32                 `json:"top_p,omitempty"`
-	Stream            *bool                    `json:"stream,omitempty"`
-	Stop              []string                 `json:"stop,omitempty"`
-	FrequencyPenalty  *float32                 `json:"frequency_penalty,omitempty"`
-	LogitBias         map[string]int           `json:"logit_bias,omitempty"`
-	LogProbs          *bool                    `json:"logprobs,omitempty"`
-	TopLogProbs       *int                     `json:"top_logprobs,omitempty"`
-	User              *string                  `json:"user,omitempty"`
-	FunctionCall      interface{}              `json:"function_call,omitempty"`
-	Tools             []*Tool                  `json:"tools,omitempty"`
-	ToolChoice        interface{}              `json:"tool_choice,omitempty"`
-	StreamOptions     *StreamOptions           `json:"stream_options,omitempty"`
-	PresencePenalty   *float32                 `json:"presence_penalty,omitempty"`
-	RepetitionPenalty *float32                 `json:"repetition_penalty,omitempty"`
-	N                 *int                     `json:"n,omitempty"`
-	ResponseFormat    *ResponseFormat          `json:"response_format,omitempty"`
-	ParallelToolCalls *bool                    `json:"parallel_tool_calls,omitempty"`
-	ServiceTier       *string                  `json:"service_tier,omitempty"`
-	Thinking          *Thinking                `json:"thinking,omitempty"`
+	Model               string                   `json:"model"`
+	Messages            []*ChatCompletionMessage `json:"messages"`
+	MaxTokens           *int                     `json:"max_tokens,omitempty"`
+	Temperature         *float32                 `json:"temperature,omitempty"`
+	TopP                *float32                 `json:"top_p,omitempty"`
+	Stream              *bool                    `json:"stream,omitempty"`
+	Stop                []string                 `json:"stop,omitempty"`
+	FrequencyPenalty    *float32                 `json:"frequency_penalty,omitempty"`
+	LogitBias           map[string]int           `json:"logit_bias,omitempty"`
+	LogProbs            *bool                    `json:"logprobs,omitempty"`
+	TopLogProbs         *int                     `json:"top_logprobs,omitempty"`
+	User                *string                  `json:"user,omitempty"`
+	FunctionCall        interface{}              `json:"function_call,omitempty"`
+	Tools               []*Tool                  `json:"tools,omitempty"`
+	ToolChoice          interface{}              `json:"tool_choice,omitempty"`
+	StreamOptions       *StreamOptions           `json:"stream_options,omitempty"`
+	PresencePenalty     *float32                 `json:"presence_penalty,omitempty"`
+	RepetitionPenalty   *float32                 `json:"repetition_penalty,omitempty"`
+	N                   *int                     `json:"n,omitempty"`
+	ResponseFormat      *ResponseFormat          `json:"response_format,omitempty"`
+	ParallelToolCalls   *bool                    `json:"parallel_tool_calls,omitempty"`
+	ServiceTier         *string                  `json:"service_tier,omitempty"`
+	Thinking            *Thinking                `json:"thinking,omitempty"`
+	MaxCompletionTokens *int                     `json:"max_completion_tokens,omitempty"`
 }
 
 func (r CreateChatCompletionRequest) MarshalJSON() ([]byte, error) {
