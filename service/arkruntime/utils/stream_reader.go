dc8f13f570824c31b5f6cdb1b6bb556d7b4d0a31
liuhuiqi.7 <liuhuiqi.7@bytedance.com>
2025-12-24 18:48:06 +0800

    feat(e2ee): init key noce

--- service/arkruntime/utils/stream_reader.go
+++ service/arkruntime/utils/stream_reader.go
@@ -24,6 +24,7 @@ type ChatCompletionStreamReader struct {
 	Response       *http.Response
 	ErrAccumulator ErrorAccumulator
 	Unmarshaler    Unmarshaler
+	KeyNonce       []byte
 
 	model.HttpHeader
 }
